0e840c8cb10e903674a1e0ea90045a8925e6bdb4
Dominique <dominique.orban@gmail.com>
2021-04-21 13:55:27 -0400

    API is now contained in Reference

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,6 @@ makedocs(
   sitename = "RipQP.jl",
   pages = ["Home" => "index.md",
            "Tutorial" => "tutorial.md",
-           "API" => "api.md",
            "Reference" => "reference.md",
           ]
 )
