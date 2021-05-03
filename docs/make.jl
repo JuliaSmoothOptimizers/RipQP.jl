bd9e511e7a10b82903429ca9f8fd05cd8a4e5f18
Geoffroy Leconte <gleconte50@gmail.com>
2021-05-03 14:38:03 -0400

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
     prettyurls = get(ENV, "CI", nothing) == "true",
   ),
   sitename = "RipQP.jl",
-  pages = ["Home" => "index.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
+  pages = ["Home" => "index.md", "API" => "API.md", "Tutorial" => "tutorial.md", "Reference" => "reference.md"],
 )
 
 deploydocs(
