14c2f0ea130b2661bf81c578c386d0106e0c242c
geoffroyleconte <geoffroyleconte@users.noreply.github.com>
2021-09-15 16:04:52 +0000

    :robot: Format .jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -406,11 +406,7 @@ end
   stats4 = ripqp(
     QuadraticModel(qps4),
     display = false,
-    iconf = InputConfig(
-      sp = K1KrylovParams(),
-      solve_method = :IPF,
-      history = true,
-    ),
+    iconf = InputConfig(sp = K1KrylovParams(), solve_method = :IPF, history = true),
     itol = InputTol(max_iter = 50, max_time = 20.0, ϵ_rc = 1.0e-4, ϵ_rb = 1.0e-4, ϵ_pdd = 1.0e-4),
   )
   @test isapprox(stats4.objective, -4.6475314286e02, atol = 1e-2)
@@ -420,11 +416,7 @@ end
   stats4 = ripqp(
     QuadraticModel(qps4),
     display = false,
-    iconf = InputConfig(
-      sp = K1KrylovParams(uplo = :U),
-      solve_method = :PC,
-      history = true,
-    ),
+    iconf = InputConfig(sp = K1KrylovParams(uplo = :U), solve_method = :PC, history = true),
     itol = InputTol(max_iter = 50, max_time = 20.0, ϵ_rc = 1.0e-4, ϵ_rb = 1.0e-4, ϵ_pdd = 1.0e-4),
   )
   @test isapprox(stats4.objective, -4.6475314286e02, atol = 1e-2)
