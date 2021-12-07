2933fa265f33d24d8ed25a810346a9f287470ae9
Geoffroy Leconte <gleconte50@gmail.com>
2021-12-07 13:22:50 -0500

    issue 1.3

--- src/iterations/solvers/Krylov_utils.jl
+++ src/iterations/solvers/Krylov_utils.jl
@@ -128,7 +128,7 @@ ksolve!(
 ) where {T, S} =
   trimr!(KS, A, ξ1, ξ2, M = M, N = N, flip = true, verbose = verbose, atol = atol, rtol = rtol)
 
-ksolve!(
+function ksolve!(
   KS::GpmrSolver{T, S},
   A,
   ξ1::AbstractVector{T},
@@ -138,8 +138,10 @@ ksolve!(
   verbose::Integer = 0,
   atol::T = T(sqrt(eps(T))),
   rtol::T = T(sqrt(eps(T))),
-) where {T, S} =
-  gpmr!(KS, A, A', ξ1, ξ2, C = sqrt.(M), D = sqrt(N), E = sqrt.(M), F = sqrt(N), λ = -one(T), verbose = verbose, atol = atol, rtol = rtol)
+) where {T, S}
+  sqrtδI = sqrt(N.λ) * I
+  return gpmr!(KS, A, A', ξ1, ξ2, C = sqrt.(M), D = sqrtδI, E = sqrt.(M), F = sqrtδI, λ = -one(T), verbose = verbose, atol = atol, rtol = rtol)
+end
 
 function kscale!(rhs::AbstractVector{T}) where {T <: Real}
   rhsNorm = norm(rhs)
