e12f4968267364d3c2ea5d52cca2ac30e653296e
geoffroyleconte <geoffroyleconte@users.noreply.github.com>
2022-03-07 21:29:22 +0000

    :robot: Format .jl files

--- test/solvers/test_augmented.jl
+++ test/solvers/test_augmented.jl
@@ -76,7 +76,12 @@ end
         QuadraticModel(qps3),
         display = false,
         iconf = InputConfig(
-          sp = K2KrylovParams(uplo = :U, kmethod = kmethod, preconditioner = precond, form_mat = true),
+          sp = K2KrylovParams(
+            uplo = :U,
+            kmethod = kmethod,
+            preconditioner = precond,
+            form_mat = true,
+          ),
         ),
         itol = InputTol(
           max_iter = 50,
