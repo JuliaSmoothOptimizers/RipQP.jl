aec7e1d5c9874fd3a85393eea06d0c6846868a23
Geoffroy Leconte <gleconte50@gmail.com>
2022-08-16 14:41:47 -0400

    change uplo

--- src/iterations/solvers/hslfact_utils.jl
+++ src/iterations/solvers/hslfact_utils.jl
@@ -6,7 +6,7 @@ get_mat_QPData(
   sp::K2LDLParams{T0, F},
 ) where {T, T0, F <: HSLMA57Fact} = A, Symmetric(H, sp.uplo)
 
-get_uplo(fact_alg::HSLMA57Fact) = :U
+get_uplo(fact_alg::HSLMA57Fact) = :L
 
 init_fact(K::Symmetric{T, SparseMatrixCOO{T, Int}}, fact_alg::HSLMA57Fact) where {T} =
   ma57_coord(K.data.rows, K.data.cols, K.data.vals, sqd = true)
