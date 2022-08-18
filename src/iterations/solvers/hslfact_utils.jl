c32173e4e3d819b915a8e8db846f829c63b5612e
Geoffroy Leconte <gleconte50@gmail.com>
2022-08-18 17:09:23 -0400

    ma57 modifs

--- src/iterations/solvers/hslfact_utils.jl
+++ src/iterations/solvers/hslfact_utils.jl
@@ -10,15 +10,20 @@ get_mat_QPData(
 
 get_uplo(fact_alg::HSLMA57Fact) = :L
 
-init_fact(K::Symmetric{T, SparseMatrixCOO{T, Int}}, fact_alg::HSLMA57Fact) where {T} =
-  ma57_coord(size(K, 1), K.data.rows, K.data.cols, K.data.vals, sqd = true)
-
-function generic_factorize!(K::Symmetric{T, SparseMatrixCOO{T, Int}}, K_fact::Ma57) where {T}
-  K_fact.vals .= K.data.vals
-  ma57_factorize(K_fact)
+mutable struct Ma57Factorization{T}
+  ma57::Ma57{T}
+  work::Vector{T}
 end
 
-LDLFactorizations.factorized(K_fact::Ma57) = true
+init_fact(K::Symmetric{T, SparseMatrixCOO{T, Int}}, fact_alg::HSLMA57Fact) where {T} =
+  Ma57Factorization(ma57_coord(size(K, 1), K.data.rows, K.data.cols, K.data.vals, sqd = true), Vector{T}(undef, size(K, 1)))
+
+function generic_factorize!(K::Symmetric{T, SparseMatrixCOO{T, Int}}, K_fact::Ma57Factorization{T}) where {T}
+  K_fact.ma57.vals .= K.data.vals
+  ma57_factorize!(K_fact.ma57)
+end
+
+LDLFactorizations.factorized(K_fact::Ma57Factorization) = true
 
 import LinearAlgebra.ldiv!
-ldiv!(K_fact::Ma57, dd::AbstractVector) = ma57_solve!(K_fact, dd)
+ldiv!(K_fact::Ma57Factorization, dd::AbstractVector) = ma57_solve!(K_fact.ma57, dd, K_fact.work)
