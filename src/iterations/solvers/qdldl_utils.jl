ba2f3b768e6df303019a90bbc27c98e8b68f4d2d
Geoffroy Leconte <gleconte50@gmail.com>
2022-08-19 15:18:58 -0400

    try catch

--- src/iterations/solvers/qdldl_utils.jl
+++ src/iterations/solvers/qdldl_utils.jl
@@ -6,17 +6,27 @@ mutable struct QDLDLFactorization{T}
   F::QDLDL.QDLDLFactorisation{T, Int}
   initialized::Bool
   diagindK::Vector{Int}
+  factorized::Bool
 end
 
 init_fact(K::Symmetric{T, SparseMatrixCSC{T, Int}}, fact_alg::QDLDLFact) where {T} =
-  QDLDLFactorization(QDLDL.qdldl(K.data), false, get_diagind_K(K))
+  QDLDLFactorization(QDLDL.qdldl(K.data), false, get_diagind_K(K), true)
 
 function generic_factorize!(K::Symmetric{T, SparseMatrixCSC{T, Int}}, K_fact::QDLDLFactorization{T}) where {T}
-  QDLDL.update_values!(K_fact.F, K_fact.diagindK, view(K.data.nzval, K_fact.diagindK))
-  QDLDL.refactor!(K_fact.F)
+  if K_fact.initialized
+    QDLDL.update_values!(K_fact.F, K_fact.diagindK, view(K.data.nzval, K_fact.diagindK))
+    K_fact.factorized = false
+    try
+      QDLDL.refactor!(K_fact.F)
+      K_fact.factorized = true
+    catch
+      K_fact.factorized = false
+    end
+  end
+  !K_fact.initialized && (K_fact.initialized = true)
 end
 
-LDLFactorizations.factorized(K_fact::QDLDLFactorization) = true
+LDLFactorizations.factorized(K_fact::QDLDLFactorization) = K_fact.factorized
 
 function ldiv!(K_fact::QDLDLFactorization, dd::AbstractVector)
   QDLDL.solve!(K_fact.F, dd)
