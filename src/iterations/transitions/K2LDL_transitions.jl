b97215189d6f75c78e9e9cbf45fe8488a3590fe5
Geoffroy Leconte <gleconte50@gmail.com>
2022-10-21 14:38:55 -0400

    switch transition

--- src/iterations/transitions/K2LDL_transitions.jl
+++ src/iterations/transitions/K2LDL_transitions.jl
@@ -18,12 +18,7 @@ function convertpad(
   )
 
   if pad.regu.regul == :classic
-    if T == Float64 && typeof(sp_new) == Nothing
-      pad.regu.ρ_min = 1e-5 * sqrt(eps())
-      pad.regu.δ_min = 1e0 * sqrt(eps())
-    else
-      pad.regu.ρ_min, pad.regu.δ_min = T(sqrt(eps(T)) * 1e1), T(sqrt(eps(T)) * 1e1)
-    end
+      pad.regu.ρ_min, pad.regu.δ_min = T(sqrt(eps(T))), T(sqrt(eps(T)))
   elseif pad.regu.regul == :dynamic
     pad.regu.ρ, pad.regu.δ = T(eps(T)^(3 / 4)), T(eps(T)^(0.45))
     pad.K_fact.LDL.r1, pad.K_fact.LDL.r2 = -pad.regu.ρ, pad.regu.δ
