735fe0ced9ab356f39d4dc423d74d9d3ed2b3ba8
Geoffroy Leconte <gleconte50@gmail.com>
2022-10-24 23:41:00 -0400

    update regu strat

--- src/iterations/regularization.jl
+++ src/iterations/regularization.jl
@@ -41,50 +41,33 @@ function update_regu_diagK2!(
   regu::Regularization{T},
   K::Symmetric{<:Real, <:SparseMatrixCSC},
   diagind_K,
+  μ::T,
   nvar::Int,
   itd::IterData,
   cnts::Counters;
   safety_dist_bnd::Bool = true,
 ) where {T}
-  update_regu_diagK2!(
-    regu,
-    K.data.nzval,
-    diagind_K,
-    nvar,
-    itd.pdd,
-    itd.l_pdd,
-    itd.mean_pdd,
-    cnts,
-    safety_dist_bnd,
-  )
+  update_regu_diagK2!(regu, K.data.nzval, diagind_K, μ, nvar, itd.pdd, itd.l_pdd, itd.mean_pdd, cnts, safety_dist_bnd)
 end
 
 function update_regu_diagK2!(
   regu::Regularization{T},
   K::Symmetric{<:Real, <:SparseMatrixCOO},
   diagind_K,
+  μ,
   nvar::Int,
   itd::IterData,
   cnts::Counters;
   safety_dist_bnd::Bool = false,
 ) where {T}
-  update_regu_diagK2!(
-    regu,
-    K.data.vals,
-    diagind_K,
-    nvar,
-    itd.pdd,
-    itd.l_pdd,
-    itd.mean_pdd,
-    cnts,
-    safety_dist_bnd,
-  )
+  update_regu_diagK2!(regu, K.data.vals, diagind_K, μ, nvar, itd.pdd, itd.l_pdd, itd.mean_pdd, cnts, safety_dist_bnd)
 end
 
 function update_regu_diagK2!(
   regu::Regularization{T},
   K_nzval::AbstractVector{T},
   diagind_K,
+  μ::T,
   nvar::Int,
   pdd::T,
   l_pdd::Vector{T},
@@ -94,37 +77,43 @@ function update_regu_diagK2!(
 ) where {T}
   l_pdd[cnts.k % 6 + 1] = pdd
   mean_pdd = mean(l_pdd)
-
   if safety_dist_bnd
     if T == Float64 &&
-       regu.regul == :classic &&
-       cnts.k > 10 &&
-       mean_pdd != zero(T) &&
-       std(l_pdd ./ mean_pdd) < T(1e-2) &&
-       cnts.c_pdd < 5
+      regu.regul == :classic &&
+      cnts.k > 10 &&
+      # mean_pdd != zero(T) &&
+      # std(l_pdd ./ mean_pdd) < T(1e-2) &&
+      μ ≤ eps(T) &&
+      cnts.c_pdd < 20
       regu.δ_min /= 10
       regu.δ /= 10
+      regu.ρ_min *= 10
+      regu.ρ *= 10
       cnts.c_pdd += 1
     end
     if T == Float64 &&
-       regu.regul == :classic &&
-       cnts.k > 10 &&
-       cnts.c_catch <= 1 &&
-       regu.δ_min >= eps(T)^(4 / 5) &&
-       @views minimum(K_nzval[diagind_K[1:nvar]]) < -one(T) / regu.δ / T(1e-6)
+      regu.regul == :classic &&
+      cnts.k > 10 &&
+      cnts.c_catch <= 3 &&
+      regu.δ_min >= eps(T)^(4 / 5) &&
+      @views minimum(K_nzval[diagind_K[1:nvar]]) < -one(T) / regu.δ / T(1e-6)
+      if cnts.c_catch ≥ 2
+        regu.ρ /= 10
+        regu.ρ_min /= 10
+      end
       regu.δ /= 10
       regu.δ_min /= 10
       cnts.c_pdd += 1
     elseif !cnts.last_sp &&
-           cnts.c_pdd <= 2 &&
-           cnts.k ≥ 5 &&
-           @views minimum(K_nzval[diagind_K[1:nvar]]) < -one(T) / eps(T) &&
+          cnts.c_pdd <= 2 &&
+          cnts.k ≥ 5 &&
+          @views minimum(K_nzval[diagind_K[1:nvar]]) < -one(T) / eps(T) &&
                   @views maximum(K_nzval[diagind_K[1:nvar]]) > -one(T) / 10
       regu.regul == :classic && return 1
     elseif T == Float128 &&
-           cnts.k > 10 &&
-           cnts.c_catch <= 1 &&
-           @views minimum(K_nzval[diagind_K[1:nvar]]) < -one(T) / regu.δ / T(1e-15)
+          cnts.k > 10 &&
+          cnts.c_catch <= 1 &&
+          @views minimum(K_nzval[diagind_K[1:nvar]]) < -one(T) / regu.δ / T(1e-15)
       regu.δ /= 10
       regu.δ_min /= 10
       cnts.c_pdd += 1
