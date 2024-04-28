19e1a6e5bd6b9a6fe04b2db5108120ceb658bf90
Geoffroy Leconte <gleconte50@gmail.com>
2024-04-28 23:39:26 +0200

    test with bigfloats

--- test/test_multi.jl
+++ test/test_multi.jl
@@ -83,32 +83,33 @@ end
     @test stats1.status == :first_order
   end
 
-  # qm128_1 = createQuadraticModelT(qps1, T = Double64)
-  # stats1 = ripqp(
-  #   qm128_1,
-  #   mode = :multi,
-  #   solve_method = IPF(),
-  #   sp = K2KrylovParams(
-  #     uplo = :U,
-  #     form_mat = true,
-  #     equilibrate = false,
-  #     preconditioner = LDL(T = Float64),
-  #     ρ_min = sqrt(eps()),
-  #     δ_min = sqrt(eps()),
-  #   ),
-  #   sp2 = K2KrylovParams{Double64}(
-  #     uplo = :U,
-  #     form_mat = true,
-  #     equilibrate = false,
-  #     preconditioner = LDL(T = Float64),
-  #     atol_min = Double64(1.0e-18),
-  #     rtol_min = Double64(1.0e-18),
-  #     ρ_min = sqrt(eps(Double64)),
-  #     δ_min = sqrt(eps(Double64)),
-  #   ),
-  #   display = true,
-  #   itol = InputTol(Double64, ϵ_rb = Double64(1.0e-12), ϵ_rc = Double64(1.0e-12)),
-  # )
-  # @test isapprox(stats1.objective, -1.59078179, atol = 1e-2)
-  # @test stats1.status == :first_order
+  T128 = BigFloat
+  qm128_1 = createQuadraticModelT(qps1, T = T128)
+  stats1 = ripqp(
+    qm128_1,
+    mode = :multi,
+    solve_method = IPF(),
+    sp = K2KrylovParams(
+      uplo = :U,
+      form_mat = true,
+      equilibrate = false,
+      preconditioner = LDL(T = Float64),
+      ρ_min = sqrt(eps()),
+      δ_min = sqrt(eps()),
+    ),
+    sp2 = K2KrylovParams{T128}(
+      uplo = :U,
+      form_mat = true,
+      equilibrate = false,
+      preconditioner = LDL(T = Float64),
+      atol_min = T128(1.0e-18),
+      rtol_min = T128(1.0e-18),
+      ρ_min = sqrt(eps(T128)),
+      δ_min = sqrt(eps(T128)),
+    ),
+    display = true,
+    itol = InputTol(T128, ϵ_rb = T128(1.0e-12), ϵ_rc = T128(1.0e-12)),
+  )
+  @test isapprox(stats1.objective, -1.59078179, atol = 1e-2)
+  @test stats1.status == :first_order
 end
